6649ab0c09112d9142a4f0428abc8b6e68020614
VerdaMech <verdaguergvice@gmail.com>
2025-12-08 01:28:29 -0300

    En la teoría deberiamos ir mejor

--- src/components/molecules/ProductoRow.jsx
+++ src/components/molecules/ProductoRow.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import Image from "../atoms/Image";
 import Button from "../atoms/Button";
 import Text from "../atoms/Text";
-import "../../styles/organisms/molecules/ProductRow.css"
+import "../../styles/organisms/molecules/ProductRow.css";
 
 function ProductoRow({ producto, onEdit, onDelete }) {
+  const imagenUrl =
+    producto.imagens?.length > 0
+      ? producto.imagens[0].ruta
+      : "https://via.placeholder.com/50";
   return (
     <div className="product-row">
-      
       <div className="product-row-thumb">
-        <Image 
-            src={producto.imagens?.url || ""} 
-            alt={producto.nombre_producto} 
-            width={50} 
-            height={50} 
-            />
+        <Image
+          src={imagenUrl}
+          alt={producto.nombre_producto}
+          width={50}
+          height={50}
+        />
       </div>
       <Text className="product-row-name">{producto.nombre_producto}</Text>
 
@@ -23,11 +26,14 @@ function ProductoRow({ producto, onEdit, onDelete }) {
           Editar
         </Button>
 
-        <Button variant="danger" size="sm" onClick={() => onDelete(producto.id)}>
+        <Button
+          variant="danger"
+          size="sm"
+          onClick={() => onDelete(producto.id)}
+        >
           Eliminar
         </Button>
       </div>
-
     </div>
   );
 }
