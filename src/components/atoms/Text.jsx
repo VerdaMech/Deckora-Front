441873a2933dbcf766753e26c77d64fb479be255
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-12-08 19:47:57 -0300

    Update Text.jsx
    Cambio para hacer funcionar el test

--- src/components/atoms/Text.jsx
+++ src/components/atoms/Text.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function Text({ children, variant = 'p', className }) {
-	const Tag = variant; // Puede ser 'h1', 'p', 'span', etc.
-	return <Tag className={className}>{children}</Tag>;
-}
+function Text({ children, variant = 'p', className = '' }) {
+  const tagByVariant = {
+    title: 'h1',
+    subtitle: 'h2',
+    p: 'p',
+    span: 'span',
+  };
 
+  const Tag = tagByVariant[variant] || 'p';
+
+  return <Tag className={className}>{children}</Tag>;
+}
 
 export default Text;
